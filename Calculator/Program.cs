a1f95bc2f87f79981a6d7dbadc427a98be967ac5
John-Ryan-Johnson <john4johnkids@gmail.com>
2020-07-31 11:14:15 -0500

    Add project files.

---
+++ Calculator/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter some numbers seperated by commas. (Ex: 2,4,6,8)");
+
+            string userInput = Console.ReadLine();
+
+            var numbers = userInput.Split(',');
+
+            //loop through numbers and multiply them together
+
+            int result = 1;
+
+            foreach (var number in numbers)
+            {
+                int parsedNumber = int.Parse(number);
+
+                result *= parsedNumber;
+            }
+
+
+            Console.WriteLine(result);
+
+            Console.ReadKey();
+
+        }
+    }
+}
